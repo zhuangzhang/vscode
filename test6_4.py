13f78225c9504c6738e5c0d8aab9f2de4ced72cd
“zzz <291646732@qq.com>
2017-09-28 11:35:14 +0800

    测试1

---
+++ test6_4.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 13:26:20 2017
+
+@author: zhang
+"""
+print '-'*20, 'hello zz!', '-'*20
+
+#==============================================================================
+# f = open(r'F:\python_test\test1.txt','r')
+# 
+# f.readline()
+#==============================================================================
+
+def showmenu():
+    pr="""
+    p(U)sh
+    p(O)p
+    (V)iew
+    
+    Enter choice:"""
+    choice = raw_input(pr).strip()[0].lower()
+    print '\nYou picked: [%s]' % (choice)
+    return True
+if __name__=='__main__':
+    showmenu()
+    
+#==============================================================================
+# fdict = dict((['x',1],['y',2]))
+# #for key in fdict.keys():
+# #    print 'key=%s, value=%s' % (key, fdict[key])
+# for key in fdict:
+#     print 'key=%s, value=%s' % (key, fdict[key])
+#==============================================================================
